dc5412de6529a716356f2ad67b0eea0d40e412bc
Zachary Rea <rea.zach@yahoo.com>
2023-01-27 14:29:21 -0600

    First Commit of P2
    This is the first commit of P2 to create all of the necessary files and to import the files that were given to us

---
+++ P2/p2.cpp
@@ -0,0 +1,14 @@
+/*
+Filename: p2.cpp
+Author(s): Zachary Rea and Parker Ross
+Date: 27 January 2023
+Description: 
+*/
+#include <iostream>  //allows for usage of cin, cout, and cerr
+
+#include "p2.h"
+
+using namespace std;
+
+//******************************************************************************
+
---
+++ P2/p2.h
@@ -0,0 +1,12 @@
+/*
+Filename: p2.h
+Author(s): Zachary Rea and Parker Ross
+Date: 27 January 2023
+Description: The header file for p2
+*/
+#ifndef __P2_H
+#define __P2_H
+
+#include <string>
+
+#endif
