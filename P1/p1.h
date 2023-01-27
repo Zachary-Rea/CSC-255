8ddb8e1c5bac3272e02a9e217c721ac78a695363
Zachary Rea <94135943+Zachary-Rea@users.noreply.github.com>
2023-01-27 00:39:30 -0600

    Final Commit of P1
    Final corrections before turning in p1

--- P1/p1.h
+++ P1/p1.h
@@ -11,21 +11,21 @@ Description: The header file for p1
 
 class stringList {
 private:
-    std::string *a;
-    int listCapacity;
-    int listSize;
+    std::string *a;//creates array a of string type
+    int listCapacity;//total capacity of the array 
+    int listSize;//current filled elements of the array
 public:
-    stringList (int listCapacity = 100);//Zach
-    ~stringList ();//Zach
-    bool insert (std::string text); //parker
-    bool add (std::string text); //parker 
-    bool insertAt (int index,std::string text); //parker
-    bool deleteAt (int index,std::string &text);
-    void clear();//Zach
-    void printIt() const;//Zach
-    int getIndex(std::string text) const;
-    bool readAt(int index, std::string &text) const; //parker
-    int count() const;//Zach
+    stringList (int listCapacity = 100);//(Zach)Constructor for the string list object
+    ~stringList ();//(Zach)Destructor for the string list object 
+    bool insert (std::string text);//(Parker)Inserts the given string at the top of the list
+    bool add (std::string text);//(Parker)Adds the given string to the bottom of the list
+    bool insertAt (int index,std::string text);//(Parker)Inserts the given string at the given index
+    bool deleteAt (int index,std::string &text);//(Parker)Deletes the string at the given index
+    void clear();//(Zach)Sets the current size of the list to 0 removing all entries 
+    void printIt() const;//(Zach)Prints the entire contents of the array 
+    int getIndex(std::string text) const;//(Zach)Returns the first index of the given string in the list
+    bool readAt(int index, std::string &text) const;//(Parker)Reads the string at the given index
+    int count() const;//(Zach)Returns the current count of items in the list
 };
 
 #endif
