6d78d2c74c2b8366053746e5481bfa4a5ff4a20a
Parker Ross <Parker226ross@gmail.com>
2023-02-05 19:25:58 -0600

    Update p3.cpp
    starting cpp

---
+++ P3/p3.cpp
@@ -0,0 +1,29 @@
+/*
+Filename: p3.cpp
+Author(s): Zachary Rea and Parker Ross
+Date: 27 January 2023
+Description: 
+*/
+#include <iostream>  //allows for usage of cin, cout, and cerr
+
+#include "p3.h"
+
+using namespace std;
+
+//******************************************************************************
+//Private functions
+//******************************************************************************
+//Function for decrementing passed value
+//Written by Parker
+
+      decVal(first);
+      a[first] = text;
+      if (listCount == 0){
+	last = first;
+}
+	return listCount++;
+   
+
+
+//******************************************************************************
+//Public functions
