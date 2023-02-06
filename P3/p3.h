eadf78029c2c5c73a9b41e52faaff9fd1ec3b7d0
Zachary Rea <94135943+Zachary-Rea@users.noreply.github.com>
2023-02-05 19:25:09 -0600

    Update p3.h

--- P3/p3.h
+++ P3/p3.h
@@ -2,30 +2,34 @@
 Filename: p3.h
 Author(s): Zachary Rea and Parker Ross
 Date: 5 February 2023
-Description: The header file for p1
+Description: The header file for p3
 */
 #ifndef __P1_H
 #define __P1_H
 
 #include <string>
-
-class stringList {
-private:
-    std::string *a;//creates array a of string type
-    int listCapacity;//total capacity of the array 
-    int listSize;//current filled elements of the array
-public:
-    stringList (int listCapacity = 100);//(Zach)Constructor for the string list object
-    ~stringList ();//(Zach)Destructor for the string list object 
-    bool insert (std::string text);//(Parker)Inserts the given string at the top of the list
-    bool add (std::string text);//(Parker)Adds the given string to the bottom of the list
-    bool insertAt (int index,std::string text);//(Parker)Inserts the given string at the given index
-    bool deleteAt (int index,std::string &text);//(Parker)Deletes the string at the given index
-    void clear();//(Zach)Sets the current size of the list to 0 removing all entries 
-    void printIt() const;//(Zach)Prints the entire contents of the array 
-    int getIndex(std::string text) const;//(Zach)Returns the first index of the given string in the list
-    bool readAt(int index, std::string &text) const;//(Parker)Reads the string at the given index
-    int count() const;//(Zach)Returns the current count of items in the list
+//******************************************************************************
+class cStringList {
+    private:
+        std::string *a;
+        int listCapacity;
+        int listCount;
+        int first;
+        int last;\
+        void incVal(int &value);
+        void decVal(int &value);
+    public:
+        cStringList(int capacity = 100);
+        ~cStringList();
+        bool insert(std::string text);
+        bool add(std::string text);
+        bool insertAt(int index, std::string text);
+        bool deleteAt(int index, std::string &text);
+        bool readAt(int index, std::string &text);
+        bool deleteFirst(std::string &text);
+        bool deletelast(std::string &text);
+        void clear();
+        int count() const;
+        int getIndex(std::string text);
 };
-
 #endif
