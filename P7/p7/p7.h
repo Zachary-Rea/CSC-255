6af77584216da00bbedbb2a09366e894562b6484
Zachary Rea <94135943+Zachary-Rea@users.noreply.github.com>
2023-03-08 00:27:38 -0600

    all the stuffs

---
+++ P7/p7/p7.h
@@ -0,0 +1,40 @@
+/*
+Filename: p7.h
+Author(s): Zachary Rea and Parker Ross
+Date: 7 March 2023
+Description: The header file for p7
+*/
+#ifndef __P7_H
+#define __P7_H
+
+//******************************************************************************
+class intList {
+private:
+    int *a;
+    int listCapacity;
+    int listSize;
+public:
+    //Constructors and Destructors
+    intList (int listCapacity = 100);//Zach
+    ~intList ();//Zach
+    //Public Functions 
+    bool insert (int key); //parker
+    bool add (int key); //parker 
+    bool insertAt (int index,int key); //parker
+    bool deleteAt (int index,int &key);
+    void clear();//Zach
+    void printIt(int n) const;//Zach
+    int getIndex(int key) const;
+    int readAt(int index, int &key) const; //parker
+    int capacity() const;//Zach
+    //Sorting Functions
+    void bubbleSort();
+    void selectionSort();
+    void insertionSort();
+    //Sorting Check
+    bool isSorted() const;
+    //Swap Function 
+    void swap(int *i, int *j) const;
+};
+
+#endif
