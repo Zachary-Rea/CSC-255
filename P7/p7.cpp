85320e9181b74d6a11ecaef366cc035de8d0be9d
Zachary Rea <94135943+Zachary-Rea@users.noreply.github.com>
2023-03-07 18:53:35 -0600

    stuffs

--- P7/p7.cpp
+++ P7/p7.cpp
@@ -4,120 +4,194 @@ Author(s): Zachary Rea and Parker Ross
 Date: March 7 2023
 Description:
 */
-#include <iostream>  //allows for usage of cin, cout, and cerr
-#include <string.h> //allows for usage of the string type in namespace std
-
 #include "P7.h"
+#include <iostream>
 
 using namespace std;
 
 //******************************************************************************
-//New pulbic Member Functions 
-
-void bubbleSort(){
-
+//Constructors and Destructors
+//******************************************************************************
+//Constructor
+//Written by Zach
+intList::intList (int listCapacity) {
+    this->listCapacity = listCapacity;
+    a = new int[listCapacity];
+    listSize = 0;
 }
 
-void selectionSort(){
-
-}
-
-void insertionSort(){
-
-}
-
-bool isSorted() const {
-
-}
-
-stringList::stringList (int listCapacity) { //Zach
-    this->listCapacity = listCapacity;  //set the capacity of the list to the passed in value 
-    a = new string[listCapacity];  //create a new string array of the given capacity
-    listSize = 0;  //set the current used space in the array to 0
-}
-
-stringList::~stringList () { //Zach
+//******************************************************************************
+//Destructor
+//Written by Zach
+intList::~intList() {
     delete[] a;
 }
 
-bool stringList::insert(string text) { // Parker
-bool rc = listCapacity > listSize; //Making sure we have room to move everything down to insert a new string
-if (rc) {  //if rc is true
-    for (int i = listSize; i > 0; i--){//runs through the list, starting from the end
-        a [i] = a[i-1];//moves each index over by one
+//******************************************************************************
+//Public Functions 
+//******************************************************************************
+//Function to insert a value at the start of the list
+//Written by Parker modified by Zach
+bool intList::insert(int key) {
+    bool rc = listCapacity > listSize;
+    if (rc) {
+        for (int i = listSize; i > 0; i--) {
+            a [i] = a[i-1];
+        }
+        a[0] = key;
+        listSize++;
     }
-    a[0] = text;//inserts the new string at begining
-    listSize++;
-} 
-return rc;
+    return rc;
 }
 
-bool stringList::add(string text) { // Parker
-bool rc = listCapacity > listSize;// checking to make sure we have room at the end to add a new string
-if (rc) { // if rc is true 
-    a [listSize] = text; // using list size to place the string at the end
-    listSize++;
-}
-return rc;
-}
-
-bool stringList::insertAt(int index, string text) { // Parker
-bool rc = listCapacity > listSize && index < listSize && index >= 0; 
-//Making sure we have room to insert a new string and **zach's idea** making sure we have a valid index
-if (rc) {  //if rc is true
-    for (int i = listSize; i > index; i--){//runs through the list starting from the end up to index
-        a [i] = a[i-1];//moves each index to the right
+//******************************************************************************
+//Fucntion to add an int to the end of the list
+//Written by Parker modified by Zach
+bool intList::add(int key) {
+    bool rc = listCapacity > listSize;
+    if (rc) { 
+        a [listSize] = key;
+        listSize++;
     }
-    a[index] = text;//insert the new string at the begining
-    listSize++; 
-} 
-return rc;
+    return rc;
 }
 
-bool stringList::deleteAt(int index, string &text) { //Parker
-bool rc = listSize > index && index >= 0; // **Zach's idea** prevents user from deleting anything outside of the list
-    if (rc){// if rc is true
-        text = a[index];// sets text to the value of the item deleted
-         for (int i = index; i < listSize; i++){ //runs through the list starting from the index
-            a[i] = a[i+1]; //moves the each index to the left
+//******************************************************************************
+//Function to insert a value at a given index of the list
+//Written by Parker modified by Zach
+bool intList::insertAt(int index, int key) {
+    bool rc = listCapacity > listSize && index < listSize && index >= 0; 
+    if (rc) {
+        for (int i = listSize; i > index; i--) {
+            a [i] = a[i-1];
+        }
+        a[index] = key;
+        listSize++; 
+    } 
+    return rc;
+}
+
+//******************************************************************************
+//Function to delete a value at a given index of the list
+//Written by Parker modified by Zach
+bool intList::deleteAt(int index, int &key) {
+    bool rc = listSize > index && index >= 0;
+        if (rc) {
+            key = a[index];
+            for (int i = index; i < listSize; i++) {
+                a[i] = a[i+1];
+            }
+        listSize--; 
     }
-    listSize--; 
-}
-return rc;
+    return rc;
 }
 
-void stringList::clear() { //Zach
-    listSize = 0; //set variable size to 0
+//******************************************************************************
+//Function to set the size to 0
+//Written and modified by Zach
+void intList::clear() {
+    listSize = 0;
 }
 
-void stringList::printIt() const{ //Zach
-for (int i = 0;i < listSize; i++) { //iterate through the array 
-    cout << "At pos " << i << " there is " << a[i] << "\n"; 
-    //print out the index and contents of the array 
-}
+//******************************************************************************
+//Function to print n of the list
+//Written and modified by Zach
+void intList::printIt(int n) const{
+    if (n < listSize) {
+        for (int i = 0; i < n - 1; i++) {
+            cout << "At pos " << i << " there is " << a[i];
+        }
+        cout << "At pos " << listSize << " there is " << a[listSize - 1];
+    } else {
+        for (int i = 0; i < listSize; i++) {
+            cout << "At pos " << i << " there is " << a[i];
+        }
+    }
 }
 
-int stringList::getIndex(string text) const{ //Zach
-    int rc = -1; //set the default return to -1
-    int count = 0; //set a counter for the times the string is found
-    for (int i = 0;i < listSize - 1;i++) { //iterate through the array at least once
-        if (a[i] == text && count == 0) {
+//******************************************************************************
+//Function to return the index of a given value
+//Written and modified by Zach
+int intList::getIndex(int key) const{
+    int rc = -1;
+    for (int i = 0;i < listSize - 1;i++) {
+        if (a[i] == key) {
             rc = i;
-            count++;
+            break;
         }
     }
     return rc;
 }
 
-int stringList::readAt(int index, string &text) const{ // Parker
-    int rc = -1; // if index is not true -1 will be the default value
-    if (index < listSize && index >= 0) { //**Zach's idea** making sure that the index is valid
-    rc = index; // setting rc to index if its true
-    text = a[index]; // sets text to the string at the given index
-}
+//******************************************************************************
+//Function to read the value at a given index
+//Written by Parker modified by Zach
+int intList::readAt(int index, int &key) const{
+    int rc = -1;
+    if (index < listSize && index >= 0) {
+        rc = index;
+        key = a[index];
+    }
     return rc;
 }
 
-int stringList::count() const{ //Zach
+//******************************************************************************
+//Function to return the current size of the list
+//Written and modiefied by Zach
+int intList::count() const{
     return listSize;
+}
+
+//******************************************************************************
+//Fucntion to sort the array via bubble method
+//Written by Zach
+void intList::bubbleSort() {
+    if (listSize) {
+        int i = 0;
+        int j = 1;
+        if (j < listSize - 1) {
+            if (a[i] > a[j]) {
+                int s = a[i];
+                a[i] = a[j];
+                a[j] = s;
+                i++;
+                j++;
+            } else {
+                i++;
+                j++;
+            }
+        }
+    }
+}
+
+//******************************************************************************
+//Function to sort the array via selection method 
+//Written by 
+void intList::selectionSort() {
+
+}
+
+//******************************************************************************
+//Function to sort the array via the insertion method 
+//Written by 
+void intList::insertionSort() {
+
+}
+
+//******************************************************************************
+//Function to tell if the array is currently sorted 
+//Written by Zach 
+bool intList::isSorted() const{
+    bool rc;
+    int i = 0;
+    int j = 1;
+    if ((a[i] < a[j]) && (j < listSize)) {
+        i++;
+        j++;
+        rc = true;
+    } else {
+        rc = false;
+        cout << "Index " << i << " is greater that index " << j;
+    }
+    return rc;
 }
--- P7/p7.h
+++ P7/p7.h
@@ -1,31 +1,38 @@
 /*
 Filename: p7.h
 Author(s): Zachary Rea and Parker Ross
-Date: 22 January 2023
-Description: The header file for p1
+Date: 7 March 2023
+Description: The header file for p7
 */
-#ifndef __P1_H
-#define __P1_H
+#ifndef __P7_H
+#define __P7_H
 
-#include <string>
-
-class stringList {
+//******************************************************************************
+class intList {
 private:
-    std::string *a;
+    int *a;
     int listCapacity;
     int listSize;
 public:
-    stringList (int listCapacity = 100);//Zach
-    ~stringList ();//Zach
-    bool insert (std::string text); //parker
-    bool add (std::string text); //parker 
-    bool insertAt (int index,std::string text); //parker
-    bool deleteAt (int index,std::string &text);
+    //Constructors and Destructors
+    intList (int listCapacity = 100);//Zach
+    ~intList ();//Zach
+    //Public Functions 
+    bool insert (int key); //parker
+    bool add (int key); //parker 
+    bool insertAt (int index,int key); //parker
+    bool deleteAt (int index,int &key);
     void clear();//Zach
-    void printIt() const;//Zach
-    int getIndex(std::string text) const;
-    int readAt(int index, std::string &text) const; //parker
+    void printIt(int n) const;//Zach
+    int getIndex(int key) const;
+    int readAt(int index, int &key) const; //parker
     int count() const;//Zach
+    //Sorting Functions
+    void bubbleSort();
+    void selectionSort();
+    void insertionSort();
+    //Sorting Check
+    bool isSorted() const;
 };
 
 #endif
